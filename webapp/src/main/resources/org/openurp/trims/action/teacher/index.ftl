6453e2fef3514466a61d0412d189d14e19a04ea2
mychii@qq.com <mychii@qq.com>
2014-12-03 13:07:38 +0800

    add teacherAction

---
+++ webapp/src/main/resources/org/openurp/trims/action/teacher/index.ftl
@@ -0,0 +1,9 @@
+[#ftl]
+[@b.head/]
+[@b.toolbar title=title!"教师查询"/]
+<table  class="indexpanel">
+  <tr>
+    <td class="index_content">[@b.div id="teacherlist" href="!search" /]</td>
+  </tr>
+</table>
+[@b.foot/]
--- webapp/src/main/scala/org/openurp/trims/action/TeacherAction.scala
+++ webapp/src/main/scala/org/openurp/trims/action/TeacherAction.scala
@@ -2,7 +2,12 @@ package org.openurp.trims.action
 
 import org.beangle.webmvc.entity.action.RestfulAction
 import org.openurp.base.Teacher
+import org.beangle.webmvc.api.view.View
 
 class TeacherAction extends RestfulAction[Teacher] {
+
+  override def save(): View = null
+
+  override def remove(): View = null
 
 }
