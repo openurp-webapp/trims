ff98c0022f5683c0aac190b8f1783c41c8e89210
mychii@qq.com <mychii@qq.com>
2014-12-11 14:27:23 +0800

    add some method to AbsEamsAction

--- webapp/src/main/scala/org/openurp/trims/action/AbsEamsAction.scala
+++ webapp/src/main/scala/org/openurp/trims/action/AbsEamsAction.scala
@@ -1,14 +1,29 @@
 package org.openurp.trims.action
 
-import org.beangle.webmvc.entity.action.RestfulAction
+import org.beangle.data.jpa.dao.OqlBuilder
 import org.beangle.data.model.Entity
-import org.openurp.teach.core.model.ProjectBean
+import org.beangle.webmvc.entity.action.RestfulAction
 import org.openurp.teach.core.Project
+import org.openurp.teach.lesson.Lesson
+import org.openurp.base.Department
+import org.beangle.webmvc.entity.action.AbstractRestfulAction
 
-abstract class AbsEamsAction[T <: Entity[_ <: java.io.Serializable]] extends RestfulAction[T] {
-  
+abstract class AbsEamsAction[T <: Entity[_ <: java.io.Serializable]] extends AbstractRestfulAction[T] {
+
   protected def getProject() = {
     entityDao.get(classOf[Project], new Integer(1))
+  }
+  
+  protected def getDepartments() = {
+    entityDao.findBy(classOf[Department], "teaching", Array(true))
+  }
+
+  protected def getLessonYears() = {
+    val query = OqlBuilder.from(classOf[Lesson], "l")
+    query.select("l.semester.schoolYear")
+    query.groupBy("l.semester.schoolYear")
+    query.orderBy("l.semester.schoolYear desc")
+    entityDao.search(query)
   }
 
 }
