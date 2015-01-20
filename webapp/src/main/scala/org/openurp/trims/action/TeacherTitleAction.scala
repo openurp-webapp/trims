4232c1f0fcb68192809af256ff53abccfa5a36b0
xinzhou <xin.2165@gmail.com>
2015-01-20 10:56:25 +0800

    add levelDepart

--- webapp/src/main/scala/org/openurp/trims/action/TeacherTitleAction.scala
+++ webapp/src/main/scala/org/openurp/trims/action/TeacherTitleAction.scala
@@ -34,23 +34,6 @@ class TeacherTitleAction extends AbsEamsAction {
     forward()
   }
 
-  def titleLevel(): String = {
-    val sql = """select pt.level_id, count(*)
-      from hr_base.staffs staff
-      join hr_base.staff_post_infoes spi on spi.id = staff.post_head_id
-      join hr_base.gb_professional_titles pt on pt.id = spi.title_id
-      group by pt.level_id 
-      order by count(*) desc"""
-    val query = SqlBuilder.sql(sql)
-    val datas = entityDao.search(query)
-    val map = new collection.mutable.HashMap[String, String]
-    entityDao.getAll(classOf[ProfessionalTitleLevel]).foreach(d => {
-      map.put(d.id.toString(), d.name)
-    })
-    putNamesAndValues(datas, data => map.get(data(0) + "").getOrElse("无职称级别"))
-    forward()
-  }
-
   def department(): String = {
     val tid = getInt("tid").get
     val title = entityDao.get(classOf[ProfessionalTitle], new Integer(tid))
@@ -64,6 +47,44 @@ class TeacherTitleAction extends AbsEamsAction {
       query.where("l.title.id=:tid", tid)
     }
     //    query.where("l.department.id is not null")
+    query.groupBy("l.department.id")
+    query.orderBy("count(*) desc")
+    val datas = entityDao.search(query)
+    val map = getDepartmentMap()
+    putNamesAndValues(datas, data => map(data(0).toString))
+    put("title", title)
+    forward()
+  }
+
+  def titleLevel(): String = {
+    val sql = """select pt.level_id, count(*)
+      from hr_base.staffs staff
+      join hr_base.staff_post_infoes spi on spi.id = staff.post_head_id
+      join hr_base.gb_professional_titles pt on pt.id = spi.title_id
+      where staff.state_id = 1
+      group by pt.level_id 
+      order by count(*) desc"""
+    val query = SqlBuilder.sql(sql)
+    val datas = entityDao.search(query)
+    val map = new collection.mutable.HashMap[String, String]
+    entityDao.getAll(classOf[ProfessionalTitleLevel]).foreach(d => {
+      map.put(d.id.toString(), d.name)
+    })
+    putNamesAndValues(datas, data => map.get(data(0) + ""))
+    forward()
+  }
+
+  def levelDepart(): String = {
+    val tid = getInt("tid").get
+    val title = entityDao.get(classOf[ProfessionalTitleLevel], new Integer(tid))
+    val query = OqlBuilder.from(classOf[Staff], "staff").join("staff.post.head", "l").join("l.title", "title")
+    query.where("staff.state.id = 1")
+    query.select("l.department.id, count(*) as num")
+    if ("undefined".equals(get("tid").get)) {
+      query.where("title.level.id is null")
+    } else {
+      query.where("title.level.id=:tid", tid)
+    }
     query.groupBy("l.department.id")
     query.orderBy("count(*) desc")
     val datas = entityDao.search(query)
