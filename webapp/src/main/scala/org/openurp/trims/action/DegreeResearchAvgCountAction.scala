b8596fc28db97b41f30cae96a8e52304f942b365
xinzhou <xin.2165@gmail.com>
2015-01-23 10:40:27 +0800

    fix error

--- webapp/src/main/scala/org/openurp/trims/action/DegreeResearchAvgCountAction.scala
+++ webapp/src/main/scala/org/openurp/trims/action/DegreeResearchAvgCountAction.scala
@@ -18,21 +18,30 @@ class DegreeResearchAvgCountAction extends AbsEamsAction {
   def thesis(): String = {
     val beginYear = get("beginYear")
     val endYear = get("endYear")
-    val sql = """select level_id,cast(sum(num)/count(*) as int) num from
-      (select degree.level_id,t.researcher_id,count(*) num
+    val sql = """select degree.level_id,sum(num)/count(*) num from
+      (select degree.level_id,r.person_id,count(*) num
       from sin_harvest.thesis_harvests t
       join sin_harvest.published_situations p on p.id = t.published_situation_id
       join sin_harvest.researchers r on r.id = t.researcher_id
       join hr_base.staffs staff on staff.person_id=r.person_id
       join base.gb_degrees degree on degree.id = staff.degree_id
       where staff.state_id=1 """ +
-      (if (beginYear.isDefined && Strings.isNotBlank(beginYear.get)) " and to_char(p.published_date,'YYYY') >= '" + beginYear.get + "'" else "") +
-      (if (endYear.isDefined && Strings.isNotBlank(endYear.get)) " and to_char(p.published_date,'YYYY') <= '" + endYear.get + "'" else "") +
-      """ group by degree.level_id,t.researcher_id) a
-      group by level_id
+      (if (beginYear.isDefined && Strings.isNotBlank(beginYear.get)) " and to_char(staff.employ_on,'YYYY') >= '" + beginYear.get + "'" else "") +
+      (if (endYear.isDefined && Strings.isNotBlank(endYear.get)) " and to_char(staff.employ_on,'YYYY') <= '" + endYear.get + "'" else "") +
+      """ group by degree.level_id,r.person_id) t
+      right join hr_base.staffs staff on staff.person_id=t.person_id
+      join base.gb_degrees degree on degree.id = staff.degree_id
+      where staff.state_id=1 """ +
+      (if (beginYear.isDefined && Strings.isNotBlank(beginYear.get)) " and to_char(staff.employ_on,'YYYY') >= '" + beginYear.get + "'" else "") +
+      (if (endYear.isDefined && Strings.isNotBlank(endYear.get)) " and to_char(staff.employ_on,'YYYY') <= '" + endYear.get + "'" else "") +
+      """ group by degree.level_id
+      having sum(t.num) > 0
       order by num desc"""
     val query = SqlBuilder.sql(sql)
     val thesises = entityDao.search(query)
+//    thesises.foreach(d=>{
+//      d(1)=String.format("%.4f", new java.lang.Double(d(1).toString))
+//    })
     val map = new collection.mutable.HashMap[String, String]
     entityDao.getAll(classOf[DegreeLevel]).foreach(d => {
       map.put(d.id.toString(), d.name)
@@ -46,8 +55,8 @@ class DegreeResearchAvgCountAction extends AbsEamsAction {
   def literature(): String = {
     val beginYear = get("beginYear")
     val endYear = get("endYear")
-    val sql = """select level_id,cast(sum(num)/count(*) as int) num from
-        (select degree.level_id,l.researcher_id,count(*) num
+    val sql = """select degree.level_id,sum(num)/count(*) num from
+        (select degree.level_id,r.person_id,count(*) num
         from sin_harvest.literatures l
         join sin_harvest.researchers r on r.id = l.researcher_id
         join hr_base.staffs staff on staff.person_id=r.person_id
@@ -55,11 +64,20 @@ class DegreeResearchAvgCountAction extends AbsEamsAction {
         where staff.state_id=1 """ +
         (if (beginYear.isDefined && Strings.isNotBlank(beginYear.get)) " and to_char(staff.employ_on,'YYYY') >= '" + beginYear.get + "'" else "") +
         (if (endYear.isDefined && Strings.isNotBlank(endYear.get)) " and to_char(staff.employ_on,'YYYY') <= '" + endYear.get + "'" else "") +
-        """ group by degree.level_id,l.researcher_id)a
-        group by level_id
+        """ group by degree.level_id,r.person_id)t
+        right join hr_base.staffs staff on staff.person_id=t.person_id
+        join base.gb_degrees degree on degree.id = staff.degree_id
+        where staff.state_id=1 """ +
+        (if (beginYear.isDefined && Strings.isNotBlank(beginYear.get)) " and to_char(staff.employ_on,'YYYY') >= '" + beginYear.get + "'" else "") +
+        (if (endYear.isDefined && Strings.isNotBlank(endYear.get)) " and to_char(staff.employ_on,'YYYY') <= '" + endYear.get + "'" else "") +
+        """ group by degree.level_id
+        having sum(t.num) > 0
         order by num desc"""
     val query = SqlBuilder.sql(sql)
     val literatures = entityDao.search(query)
+//    literatures.foreach(d=>{
+//      d(1)=String.format("%.4f", new java.lang.Double(d(1).toString))
+//    })
     val map = new collection.mutable.HashMap[String, String]
     entityDao.getAll(classOf[DegreeLevel]).foreach(d => {
       map.put(d.id.toString(), d.name)
--- webapp/src/main/scala/org/openurp/trims/action/TitleResearchAvgCountAction.scala
+++ webapp/src/main/scala/org/openurp/trims/action/TitleResearchAvgCountAction.scala
@@ -17,8 +17,8 @@ class TitleResearchAvgCountAction extends AbsEamsAction {
   def thesis(): String = {
     val beginYear = get("beginYear")
     val endYear = get("endYear")
-    val sql = """select title_id,cast(sum(num)/count(*) as int) num from
-      (select spi.title_id,t.researcher_id,count(*) num
+    val sql = """select spi.title_id,sum(t.num)/count(*) num from
+      (select spi.title_id,r.person_id,count(*) num
       from sin_harvest.thesis_harvests t
       join sin_harvest.published_situations p on p.id = t.published_situation_id
       join sin_harvest.researchers r on r.id = t.researcher_id
@@ -27,8 +27,11 @@ class TitleResearchAvgCountAction extends AbsEamsAction {
       where staff.state_id=1 """ +
       (if (beginYear.isDefined && Strings.isNotBlank(beginYear.get)) " and to_char(p.published_date,'YYYY') >= '" + beginYear.get + "'" else "") +
       (if (endYear.isDefined && Strings.isNotBlank(endYear.get)) " and to_char(p.published_date,'YYYY') <= '" + endYear.get + "'" else "") +
-      """ group by spi.title_id,t.researcher_id) a
-      group by title_id
+      """ group by spi.title_id,r.person_id) t
+      right join hr_base.staffs staff on staff.person_id=t.person_id
+      join hr_base.staff_post_infoes spi on spi.id = staff.post_head_id
+      group by spi.title_id
+      having sum(t.num) > 0
       order by num desc"""
     val query = SqlBuilder.sql(sql)
     val thesises = entityDao.search(query)
@@ -36,7 +39,7 @@ class TitleResearchAvgCountAction extends AbsEamsAction {
     entityDao.getAll(classOf[ProfessionalTitle]).foreach(d => {
       map.put(d.id.toString(), d.name)
     })
-    putNamesAndValues(thesises, data => map.get(data(0) + "").getOrElse("暂定系列"))
+    putNamesAndValues(thesises, data => map.get(data(0) + "").getOrElse("无职称"))
     put("beginYear", beginYear)
     put("endYear", endYear)
     forward()
@@ -45,25 +48,28 @@ class TitleResearchAvgCountAction extends AbsEamsAction {
   def literature(): String = {
     val beginYear = get("beginYear")
     val endYear = get("endYear")
-    val sql = """select title_id,cast(sum(num)/count(*) as int) num from
-        (select spi.title_id,l.researcher_id,count(*) num
+    val sql = """select spi.title_id,sum(t.num)/count(*) num from 
+        (select spi.title_id,r.person_id,count(*) num
         from sin_harvest.literatures l
         join sin_harvest.researchers r on r.id = l.researcher_id
         join hr_base.staffs staff on staff.person_id=r.person_id
         join hr_base.staff_post_infoes spi on spi.id = staff.post_head_id
-        where staff.state_id=1  """ +
-      (if (beginYear.isDefined && Strings.isNotBlank(beginYear.get)) " and to_char(publish_date,'YYYY') >= '" + beginYear.get + "'" else "") +
-      (if (endYear.isDefined && Strings.isNotBlank(endYear.get)) " and to_char(publish_date,'YYYY') <= '" + endYear.get + "'" else "") +
-      """ group by spi.title_id,l.researcher_id)a
-      group by title_id
-      order by num desc"""
+        where staff.state_id=1 """ +
+        (if (beginYear.isDefined && Strings.isNotBlank(beginYear.get)) " and to_char(l.publish_date,'YYYY') >= '" + beginYear.get + "'" else "") +
+        (if (endYear.isDefined && Strings.isNotBlank(endYear.get)) " and to_char(l.publish_date,'YYYY') <= '" + endYear.get + "'" else "") +
+        """ group by spi.title_id,r.person_id) t
+        right join hr_base.staffs staff on staff.person_id=t.person_id
+        join hr_base.staff_post_infoes spi on spi.id = staff.post_head_id
+        group by spi.title_id
+        having sum(t.num) > 0
+        order by num desc """
     val query = SqlBuilder.sql(sql)
     val literatures = entityDao.search(query)
     val map = new collection.mutable.HashMap[String, String]
     entityDao.getAll(classOf[ProfessionalTitle]).foreach(d => {
       map.put(d.id.toString(), d.name)
     })
-    putNamesAndValues(literatures, data => map.get(data(0) + "").getOrElse("暂定系列"))
+    putNamesAndValues(literatures, data => map.get(data(0) + "").getOrElse("无职称"))
     put("beginYear", beginYear)
     put("endYear", endYear)
     forward()
